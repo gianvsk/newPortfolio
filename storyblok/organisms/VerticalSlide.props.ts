c04ca72b13de3206cb4babb51cf00cff8e1cab15
gianvsk <gianvitoscandurra@gmail.com>
2025-02-14 09:56:18 +0100

    feat: creating new storyblok component

---
+++ storyblok/organisms/VerticalSlide.props.ts
@@ -0,0 +1,10 @@
+export type VerticalSlideProps = {
+  blok: {
+    title: string;
+    subtitle: string;
+    paragraph?: string;
+    image?: any[];
+    icons?: string[];
+  };
+  isSingleImage?: boolean;
+};
---
+++ storyblok/organisms/VerticalSlide.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import type { VerticalSlideProps } from './VerticalSlide.props';
+
+  const props = defineProps<VerticalSlideProps>();
+
+  console.log({ props });
+</script>
+
+<template>
+  <div class="grid grid-cols-2 grid-rows-2 gap-6 bg-red-300 h-full w-full">
+    <slot>
+      <div class="col-start-1 row-start-1 col-span-1 row-span-1">
+        <span class="text-black">{{ blok.title }}</span>
+      </div>
+      <div class="col-start-1 row-start-2 col-span-1 row-span-1">
+        <span class="text-black">{{ blok.subtitle }}</span>
+      </div>
+    </slot>
+    <div class="col-start-2 row-start-1 col-span-1 row-span-1">
+      <span class="text-black">{{ blok.paragraph }}</span>
+    </div>
+    <div class="col-start-2 row-start-2 col-span-1 row-span-1">
+      <span class="text-black">{{ blok.title }}</span>
+    </div>
+  </div>
+</template>
